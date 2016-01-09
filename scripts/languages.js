8ef7c0b40ee5eb6be5e1e175e83982fb8cea05cb
Harriet Willmott <harriet.willmott@gmail.com>
2016-01-08 18:59:49 -0800

    converted practicalities scripts to js

---
+++ scripts/languages.js
@@ -0,0 +1,118 @@
+var names;
+var sizes;
+var angles;
+var colors;
+
+var sum = 0;
+var diameter = 300;
+var focusSlice = 0;
+var originX;
+var originY;
+
+function readXml(xmlFile)
+{
+    var xmlDoc;
+
+    if(typeof window.DOMParser != "undefined") {
+        xmlhttp=new XMLHttpRequest();
+        xmlhttp.open("GET",xmlFile,false);
+        if (xmlhttp.overrideMimeType){
+            xmlhttp.overrideMimeType('text/xml');
+        }
+        xmlhttp.send();
+        xmlDoc=xmlhttp.responseXML;
+    }
+    else{
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async="false";
+        xmlDoc.load(xmlFile);
+    }
+
+    var languages = xmlDoc.getElementsByTagName("language");
+    names = new Array(languages.length);
+    sizes = new Array(languages.length);
+
+    for (var i = 0; i < languages.length; i++)
+    {
+        names[i] = languages[i].getAttribute("name");
+        sizes[i] = parseInt(languages[i].getAttribute("strength"));
+
+        sum = sum + sizes[i];
+    }
+
+}
+
+function sketchProc(processing)
+{
+    processing.setup = function()
+    {
+        processing.size(500,350);
+        processing.noStroke();
+
+        angles = new Array(names.length);
+        var angle = 0;
+        for (var i = 0; i < names.length; i++)
+        {
+            angle = angle + sizes[i] / sum * processing.TWO_PI;
+            angles[i] = angle; 
+        }
+        initColors();
+        originX = 180;
+        originY = 180;
+    }
+
+    processing.draw = function()
+    {
+        processing.background(300);
+
+        var angle = 0;
+
+        if (processing.dist(processing.mouseX, processing.mouseY, originX, originY) < diameter / 2)
+        {
+            var a = processing.atan2(processing.mouseY - originY, processing.mouseX - originX);
+            a = (a + processing.TWO_PI) % processing.TWO_PI;
+            var indx = 0;
+            for (var i = 0; i < angles.length; i++)
+            {
+                if (a < angles[i]) break;
+                indx++;
+            }
+            focusSlice = indx;
+        }
+
+        for (var i = 0; i < names.length; i++)
+        {
+            processing.fill(colors[i % (colors.length)]);
+            if (i == focusSlice)
+            {
+                processing.arc(originX, originY, diameter + 20, diameter + 20, angle, angles[i]);
+                processing.fill(0, 0, 0);
+                processing.textSize(16);
+                processing.text(names[i], processing.width - 100, 100);
+            }
+            else
+            {
+                processing.arc(originX, originY, diameter, diameter, angle, angles[i]);
+            }
+            angle = angles[i];
+        }
+    }
+
+    function initColors()
+    {
+        colors = new Array(7);
+        colors[0] = processing.color(219, 97, 97);
+        colors[1] = processing.color(219, 144, 97);
+        colors[2] = processing.color(219, 195, 97);
+        colors[3] = processing.color(144, 219, 97);
+        colors[4] = processing.color(97, 219, 213);
+        colors[5] = processing.color(97, 109, 219);
+        colors[6] = processing.color(219, 97, 217);
+    }
+}
+
+readXml("data/languagedata.xml");
+
+var canvas = document.getElementById("canvas1");
+
+var p = new Processing(canvas, sketchProc);
---
+++ scripts/timeline.js
@@ -0,0 +1,139 @@
+var companies;
+var positions;
+var durations;
+var months;
+var descriptions;
+var colorsxml;
+
+var sum = 0;
+var startHeight = 50;
+var rectangleHeight = 20;
+var focusPadding = 5;
+var focusIndx = 0;
+
+function readXml(xmlFile)
+{
+    var xmlDoc;
+
+    if(typeof window.DOMParser != "undefined") {
+        xmlhttp=new XMLHttpRequest();
+        xmlhttp.open("GET",xmlFile,false);
+        if (xmlhttp.overrideMimeType){
+            xmlhttp.overrideMimeType('text/xml');
+        }
+        xmlhttp.send();
+        xmlDoc=xmlhttp.responseXML;
+    }
+    else{
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async="false";
+        xmlDoc.load(xmlFile);
+    }
+
+    var jobs = xmlDoc.getElementsByTagName("job");
+    companies = new Array(jobs.length);
+    positions = new Array(jobs.length);
+    durations = new Array(jobs.length);
+    months = new Array(jobs.length);
+    descriptions = new Array(jobs.length);
+    colorsxml = new Array(jobs.length);
+
+    for(var i = 0; i < jobs.length; i++)
+    {
+        companies[i] = jobs[i].getAttribute("company");
+        positions[i] = jobs[i].getAttribute("position");
+        durations[i] = jobs[i].getAttribute("duration");
+        months[i] = parseInt(jobs[i].getAttribute("months"));
+        colorsxml[i] = parseInt(jobs[i].getAttribute("color"));
+        descriptions[i] = jobs[i].textContent;
+
+        sum = sum + months[i];
+    }
+}
+
+function sketchProc(processing)
+{
+    var colors;
+
+    processing.setup = function()
+    {
+        processing.size(1000,400);
+        processing.noStroke();
+        initColors()
+    }
+
+    processing.draw = function()
+    {
+        processing.background(300);
+        var x = 0;
+        for(var i = 0; i < companies.length; i++)
+        {
+            var rectangleWidth = months[i] / sum * processing.width;
+            if (processing.mouseY >= startHeight && processing.mouseY <= (startHeight + rectangleHeight)
+                && processing.mouseX > x && processing.mouseX < (x + rectangleWidth))
+            {
+                focusIndx = i;
+            }
+            drawRectangle(i, x, rectangleWidth);
+            drawLabel(i, x);
+            x = x + rectangleWidth;
+        }
+    }
+
+    function drawRectangle(i, x, rectangleWidth)
+    {
+        processing.fill(colors[colorsxml[i]]);
+        if (i == focusIndx)
+        {
+            processing.rect(x, startHeight + focusPadding, rectangleWidth, rectangleHeight);
+            drawContent(i);
+        }
+        else
+        {
+            processing.rect(x, startHeight, rectangleWidth, rectangleHeight);
+        }
+    }
+
+    function drawLabel(i, x)
+    {
+        processing.fill(0);
+        processing.textSize(11);
+        if (i == focusIndx)
+        {
+            processing.text(companies[i], x + 2, 50);
+        }
+        else
+        {
+            processing.text(companies[i], x + 2, 45);
+        }
+    }
+
+    function drawContent(i)
+    {
+        processing.fill(0);
+        processing.textSize(14);
+        processing.text(positions[i], 20, 120);
+        processing.text(durations[i], 20, 140);
+        processing.textSize(11);
+        processing.text(descriptions[i], 20, 160);
+    }
+
+    function initColors()
+    {
+        colors = new Array(companies.length);
+        colors[0] = processing.color(219, 97, 97);
+        colors[1] = processing.color(219, 144, 97);
+        colors[2] = processing.color(219, 195, 97);
+        colors[3] = processing.color(144, 219, 97);
+        colors[4] = processing.color(97, 219, 213);
+        colors[5] = processing.color(97, 109, 219);
+        colors[6] = processing.color(219, 97, 217);
+    }
+}
+
+
+readXml("data/workexperience.xml");
+
+var canvas = document.getElementById("canvas2");
+
+var p = new Processing(canvas, sketchProc);
